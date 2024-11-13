276ea39eb77339502125b631881562da32707d05
Michelle Madeira <michellemadeira1@gmail.com>
2024-11-13 17:28:17 -0300

    Game repository, service , controller

---
+++ src/main/java/com/decsuperior/dslist/repositories/GameRepository.java
@@ -0,0 +1,9 @@
+package com.decsuperior.dslist.repositories;
+
+import com.decsuperior.dslist.entities.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+
+
+}
